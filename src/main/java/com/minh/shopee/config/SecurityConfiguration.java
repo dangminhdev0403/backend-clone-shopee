b5ae4c3ceea7fab339322324c9243cec5c9cda77
Your Name <you@example.com>
2025-05-13 14:56:43 +0700

    update

--- src/main/java/com/minh/shopee/config/SecurityConfiguration.java
+++ src/main/java/com/minh/shopee/config/SecurityConfiguration.java
@@ -1,6 +1,8 @@
 package com.minh.shopee.config;
 
 import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Stream;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -16,12 +18,22 @@ public class SecurityConfiguration {
         public SecurityFilterChain filterChain(HttpSecurity http,
                         CustomAuthenticationEntryPoint customAuthenticationEntryPoint)
                         throws Exception {
-                String apiPrefix = "/api/v1";
-                String[] publicPaths = { "/auth/**", "/categories/**","/api-docs/**","sư" };
+                List<String> versions = List.of("v1", "v2", "v3");
+                String apiBase = "/api";
+                // Các path công khai cho từng version
+                String[] versionedPaths = { "/categories/**", "/posts/**" };
+                // Các path chung (không version)
+                String[] commonPaths = { "/auth/**", "/swagger-ui/**", "/api-docs/**" };
 
-                String[] whiteList = Arrays.stream(publicPaths)
-                                .map(path -> path.equals("/") ? path : apiPrefix + path)
-                                .toArray(String[]::new);
+                // Gộp path công khai cho tất cả version
+                List<String> versionedWhitelist = versions.stream()
+                                .flatMap(version -> Arrays.stream(versionedPaths)
+                                                .map(path -> apiBase + "/" + version + path))
+                                .toList();
+                // Gộp tất cả lại
+                String[] whiteList = Stream.concat(
+                                versionedWhitelist.stream(),
+                                Arrays.stream(commonPaths)).toArray(String[]::new);
 
                 http
                                 .csrf(c -> c.disable())
@@ -30,9 +42,7 @@ public class SecurityConfiguration {
                                 // prettier-ignore
                                 authz.requestMatchers(whiteList)
                                                 .permitAll()
-                                                .anyRequest().authenticated()
-
-                                )
+                                                .anyRequest().authenticated())
                                 .oauth2ResourceServer(oauth2 -> oauth2.jwt(Customizer.withDefaults())
                                                 .authenticationEntryPoint(customAuthenticationEntryPoint))
                                 .formLogin(f -> f.disable())
