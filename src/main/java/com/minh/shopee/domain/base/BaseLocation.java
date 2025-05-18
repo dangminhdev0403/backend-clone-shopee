9296b310d6a6e748b7296b3ab408394f32118dda
Your Name <you@example.com>
2025-05-19 02:10:43 +0700

    update

--- src/main/java/com/minh/shopee/domain/base/BaseLocation.java
+++ src/main/java/com/minh/shopee/domain/base/BaseLocation.java
@@ -2,6 +2,7 @@ package com.minh.shopee.domain.base;
 
 import jakarta.persistence.Id;
 import jakarta.persistence.MappedSuperclass;
+import jakarta.persistence.PrePersist;
 import jakarta.validation.constraints.NotBlank;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
@@ -16,8 +17,21 @@ import lombok.experimental.SuperBuilder;
 @AllArgsConstructor
 @NoArgsConstructor
 public class BaseLocation {
+    private static final java.util.Random RANDOM = new java.util.Random();
+
     @Id
     private Long id;
     @NotBlank(message = "Name is required")
     private String name;
+
+    @PrePersist
+    public void generateId() {
+        if (id == null) {
+            long timestamp = System.currentTimeMillis();
+            long randomPart = Math.abs(RANDOM.nextLong() % 1000);
+
+            this.id = timestamp * 1000 + randomPart;
+
+        }
+    }
 }
